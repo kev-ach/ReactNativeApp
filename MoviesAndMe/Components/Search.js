4fc253181840fad8a8f0ac130d55da6ec0ae0b6a
Unknown <achiche.kevin@hotmail.fr>
2019-10-07 23:38:46 +0200

    MAJ

--- MoviesAndMe/Components/Search.js
+++ MoviesAndMe/Components/Search.js
@@ -13,6 +13,27 @@ class Search extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  // Components/Search.js
+
+import React from 'react'
+import { StyleSheet, View, TextInput, Button, Text } from 'react-native'
+
+class Search extends React.Component {
+  render() {
+    return (
+      <View style={styles.main_container}>
+        <TextInput style={styles.textinput} placeholder='Titre du film'/>
+        <Button title='Rechercher' onPress={() => {}}/>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  main_container: {
+    flex: 1,
+    marginTop: 20
+  },
   textinput: {
     marginLeft: 5,
     marginRight: 5,
