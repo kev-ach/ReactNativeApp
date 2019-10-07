b1731ee137c9e1416dedc630fc8b8c6ba0aeecb5
Unknown <achiche.kevin@hotmail.fr>
2019-10-07 21:36:14 +0200

    Creation d'un style css

--- MoviesAndMe/Components/Search.js
+++ MoviesAndMe/Components/Search.js
@@ -1,15 +1,26 @@
 import React from 'react'
-import { View, Button, TextInput } from 'react-native'
+import { StyleSheet, View, Button, TextInput } from 'react-native'
 
 class Search extends React.Component {
   render(){
     return(
-      <View style={{ marginTop: "10%"}}>
-        <TextInput placeholder="Titre du film" />
-        <Button title="Rechercher" onPress={() => {}} />
+      <View style={{ marginTop: 50}}>
+        <TextInput style={styles.textinput} placeholder="Titre du film" />
+        <Button style={{ height: 50 }} title="Rechercher" onPress={() => {}} />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  textinput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5
+  }
+})
 
 export default Search
